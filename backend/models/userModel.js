f9d3c0d22c288be074e5083fcf039b2f8cfb65f6
shaakilahammed <shaakilahammed@gmail.com>
2022-07-02 18:08:32 +0600

    backend authentication using jwtwebtoken

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema(
@@ -25,6 +26,20 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// arrow function doesn't work here, don't know why
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 export default User;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+router.post('/login', authUser);
+router.post('/', registerUser);
+router.get('/profile', protect, getUserProfile);
+
+export default router;
